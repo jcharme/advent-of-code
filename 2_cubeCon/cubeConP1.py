61d87ebfc873fd2bc62ef404e91b8185f0d453e0
Julia Ducharme <122840323+jcharme@users.noreply.github.com>
2023-12-02 18:49:24 -0500

    Stuck.

---
+++ 2_cubeCon/cubeConP1.py
@@ -0,0 +1,16 @@
+'''
+Determine which games would have been possible if the bag had been 
+loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
+What is the sum of the IDs of those games?
+
+'''
+
+import fileinput
+import re
+
+for line in fileinput.input(files ='input2.txt'):
+    id += 1
+    list = line.split(":")[1].strip()
+    
+
+    
